73c552c74586427daeab2edf3081e7d5234fc042
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-30 10:20:40 -0300

    test(controller-test): added the tracer wrapper

--- infrastructure/src/test/java/com/kaua/corebank/ControllerTest.java
+++ infrastructure/src/test/java/com/kaua/corebank/ControllerTest.java
@@ -2,11 +2,13 @@ package com.kaua.corebank;
 
 import com.kaua.corebank.infrastructure.configurations.SecurityConfig;
 import com.kaua.corebank.infrastructure.idempotency.gateways.InMemoryIdempotencyKeyGateway;
+import com.kaua.corebank.infrastructure.wrapper.TracerWrapperOtel;
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import java.lang.annotation.*;
 
@@ -15,7 +17,8 @@ import java.lang.annotation.*;
 @Inherited
 @ActiveProfiles("test-integration")
 @WebMvcTest
-@Import({SecurityConfig.class, InMemoryIdempotencyKeyGateway.class, ObservationTest.OpenTelemetryTestConfig.class, IntegrationTestConfig.class})
+@TestPropertySource(properties = "application.otel.memory-exporter=true")
+@Import({SecurityConfig.class, InMemoryIdempotencyKeyGateway.class, ObservationTest.OpenTelemetryTestConfig.class, IntegrationTestConfig.class, TracerWrapperOtel.class})
 @Tag("integrationTest")
 public @interface ControllerTest {
 

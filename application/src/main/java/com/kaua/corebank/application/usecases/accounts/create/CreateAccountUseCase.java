2ca275922c20bf19751f3e3fb14f8eb79673566a
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-30 10:14:05 -0300

    feat(create-account-usecase): added the create account usecase abstract class

---
+++ application/src/main/java/com/kaua/corebank/application/usecases/accounts/create/CreateAccountUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.corebank.application.usecases.accounts.create;
+
+import com.kaua.corebank.application.UseCase;
+
+public abstract class CreateAccountUseCase extends UseCase<CreateAccountInput, CreateAccountOutput> {
+}

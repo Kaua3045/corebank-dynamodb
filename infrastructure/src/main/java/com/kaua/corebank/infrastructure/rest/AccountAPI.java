815d7c352d29beb557d63f0b6138e011d45ea91a
Kauã Pereira <zkauacodes@hotmail.com>
2025-10-13 14:28:44 -0300

    feat(account-api): added the get account by id route method

--- infrastructure/src/main/java/com/kaua/corebank/infrastructure/rest/AccountAPI.java
+++ infrastructure/src/main/java/com/kaua/corebank/infrastructure/rest/AccountAPI.java
@@ -2,15 +2,14 @@ package com.kaua.corebank.infrastructure.rest;
 
 import com.kaua.corebank.infrastructure.accounts.req.CreateAccountRequest;
 import com.kaua.corebank.infrastructure.accounts.res.CreateAccountResponse;
+import com.kaua.corebank.infrastructure.accounts.res.GetAccountByIdResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Accounts", description = "Account management API")
 @RequestMapping("/v1/accounts")
@@ -28,4 +27,17 @@ public interface AccountAPI {
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     ResponseEntity<CreateAccountResponse> createAccount(@RequestBody CreateAccountRequest input);
+
+    @GetMapping(
+            path = "/{accountId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get account by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Account successfully retrieved"),
+            @ApiResponse(responseCode = "400", description = "A validation error was observed"),
+            @ApiResponse(responseCode = "404", description = "Account not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<GetAccountByIdResponse> getAccountById(@PathVariable(name = "accountId") String accountId);
 }
